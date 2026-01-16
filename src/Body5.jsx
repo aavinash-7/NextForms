e1c8d3ce7e959f9a28e9ce4e9cc5b040f48663a0
aavinash_7 <aajava20@gmail.com>
2026-01-16 20:30:14 +0530

    commit 13

--- src/Body5.jsx
+++ src/Body5.jsx
@@ -5,30 +5,33 @@ function Body5() {
     return(
     
     <div className="picp" style={{}}>
-    <h2 style={{color:"black",margin:"6rem"}}>How It Works</h2>
+    <h2 style={{color:"black",margin:"6rem auto"}}>How It Works</h2>
     
-        <div style={{display:"flex",justifyContent:"center",alignItems:"center"}} >
+        <div className='hiw' style={{display:"flex",justifyContent:"center",alignItems:"center"}} >
 
             <div className="pic imh1" style={{}}>
                 <img className='imgs' src={img1} alt="" />
-                <h4 className='con' style={{color:"black"}}>1.Connect</h4>
-                <p className='pcon' style={{color:"black"}}>send your details on whatsapp</p>
+                <div> <h4 className='con' style={{color:"black"}}>1.Connect</h4>
+                <p className='pcon' style={{color:"black"}}>send your details on whatsapp</p></div>
+               
             </div>
 
             <div className='line' ></div>
 
             <div className="pic imh2" style={{}}>
                 <img className='imgs' src={img2} alt="" />
-                <h4 className='con' style={{color:"black"}}>2.Verify</h4>
-                <p className='pcon' style={{color:"black"}}>Review your draft for accuracy</p>
+                <div><h4 className='con' style={{color:"black"}}>2.Verify</h4>
+                <p className='pcon' style={{color:"black"}}>Review your draft for accuracy</p></div>
+                
             </div>
 
             <div className='line'></div>
 
             <div className="pic imh3" style={{}}>
                 <img className='imgs' src={img3} alt="" />
-                 <h4 className='con' style={{color:"black"}}>3.Success</h4>
-                <p className='pcon' style={{color:"black"}}>Get your official soft receipt</p>
+                <div> <h4 className='con' style={{color:"black"}}>3.Success</h4>
+                <p className='pcon' style={{color:"black"}}>Get your official soft receipt</p></div>
+                
             </div>
         </div>
     </div>);
